e9fb6ba2bdd6ed286026ba8552c4d275643bcf03
[pany] <[pany@ersoft.cn]>
2017-04-12 18:43:57 +0800

    update

--- Message/db/helper.py
+++ Message/db/helper.py
@@ -1,8 +1,10 @@
 from sqlalchemy import Column
 from sqlalchemy import create_engine
+from sqlalchemy.sql import expression
 from sqlalchemy import desc
 from sqlalchemy.types import CHAR
 from sqlalchemy.types import Integer
+from sqlalchemy.types import Float
 from sqlalchemy.types import String
 from sqlalchemy.types import VARCHAR
 from sqlalchemy.types import TIMESTAMP
@@ -10,9 +12,9 @@ from sqlalchemy.types import Text
 from sqlalchemy.types import Date
 from sqlalchemy.orm.exc import MultipleResultsFound
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.dialects.mysql import TINYINT
-from sqlalchemy.dialects.mysql import INTEGER
-from sqlalchemy.dialects.mysql import BIGINT
+from sqlalchemy.dialects.postgresql import VARCHAR
+from sqlalchemy.dialects.postgresql import SMALLINT
+from sqlalchemy.dialects.postgresql import INTEGER
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import func
@@ -31,6 +33,7 @@ import string
 import hashlib
 import uuid
 import os
+import time
 
 from Message.config import database_config
 user = database_config.get('user', 'postgres')
@@ -46,3 +49,18 @@ engine = create_engine('postgresql://{user}:{password}@{host}:{port}/{db_name}'.
 base = declarative_base()
 session = sessionmaker(bind=engine)
 
+
+class Task(base):
+
+    __tablename__ = 'task'
+
+    id = Column(INTEGER, primary_key=True)
+    type = Column(VARCHAR)
+    status = Column(SMALLINT)
+    failed_reason = Column(VARCHAR)
+    create_time = Column(TIMESTAMP, server_default=expression.text('CURRENT_TIMESTAMP(3)'))
+    update_time = Column(TIMESTAMP, server_default=expression.text('CURRENT_TIMESTAMP(3)'))
+
+
+if __name__ == '__main__':
+    base.metadata.create_all(engine)
